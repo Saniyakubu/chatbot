9b0d1bb770e2ade97b9f71782863436ba2ca138f
sani <ysani095@gmail.com>
2024-05-05 16:44:07 +0800

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -66,7 +66,7 @@ function App() {
   const sentMessage: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:2000/ask/ai", {
+      const res = await axios.post("https://chatbot-5thp.onrender.com/ask/ai", {
         history,
         message: msg,
       });
