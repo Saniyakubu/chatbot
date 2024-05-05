c9ef849416c64b826b6629148241cf99afe140b9
sani <ysani095@gmail.com>
2024-05-06 04:42:09 +0800

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
-import { CornerDownLeft, Mic, Paperclip } from "lucide-react";
+import { CornerDownLeft } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-  TooltipProvider,
-} from "@/components/ui/tooltip";
-import axios from "axios";
 import { FormEventHandler, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [history, setHistory] = useState<any[]>([]);
@@ -51,88 +45,64 @@ function App() {
   };
 
   return (
-    <div className="flex items-center h-screen">
-      <main className="grid gap-4 overflow-auto w-[90%] lg:w-[60%] mx-auto">
-        <div className="relative flex flex-col justify-between p-4 border h-[700px] rounded-xl bg-muted/50">
-          <Badge variant="outline" className="absolute right-3 top-3">
-            Output
-          </Badge>
-          <ul className="flex flex-col gap-5 p-5 pb-16 mb-20">
-            {history.map((text: any, index) => {
-              return (
-                <>
-                  {text.role === "user" ? (
-                    <li key={index} className="flex flex-row">
-                      <div className="flex p-4 shadow-md rounded-xl bg-background">
-                        <p className="text-primary">{text.parts[0].text}</p>
-                      </div>
-                    </li>
-                  ) : (
-                    <li key={index} className="flex">
-                      <div className="flex w-3/4 p-4 shadow-md rounded-xl">
-                        <p className="text-primary">
-                          <span className="font-bold">{text.role}: </span>
-                          {text.parts[0].text}
-                        </p>
-                      </div>
-                    </li>
-                  )}
-                </>
-              );
-            })}
-          </ul>
-          <div className="bg-background">
-            <form
-              onSubmit={sentMessage}
-              className="overflow-hidden rounded-lg bg-background focus-within:ring-1 focus-within:ring-ring"
-              x-chunk="dashboard-03-chunk-1"
+    <main className="gap-4 w-[90%] lg:w-[60%] mx-auto">
+      <div className="relative w-full p-4 border mt-5 h-[700px] rounded-xl bg-muted/50">
+        <Badge variant="outline" className="absolute right-3 top-3">
+          Output
+        </Badge>
+        <ul className="flex flex-col h-full gap-5 p-5 pb-16 mb-20 overflow-y-scroll">
+          {history.map((text: any, index) => {
+            return (
+              <>
+                {text.role === "user" ? (
+                  <li key={index} className="flex flex-row">
+                    <div className="flex p-4 shadow-md rounded-xl bg-background">
+                      <p className="text-primary">{text.parts[0].text}</p>
+                    </div>
+                  </li>
+                ) : (
+                  <li key={index} className="flex">
+                    <div className="flex w-3/4 p-4 shadow-md rounded-xl">
+                      <p className="text-primary">
+                        <span className="font-bold">{text.role}: </span>
+                        {text.parts[0].text}
+                      </p>
+                    </div>
+                  </li>
+                )}
+              </>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="fixed lg:w-[60%] mx-auto bottom-5 left-0 right-0">
+        <form
+          onSubmit={sentMessage}
+          className="w-full overflow-hidden rounded-lg bg-muted/50 focus-within:ring-1 focus-within:ring-ring"
+        >
+          <Label htmlFor="message" className="sr-only">
+            Message
+          </Label>
+          <div className="flex items-center ">
+            <Textarea
+              id="message"
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+              placeholder="Type your message here..."
+              className="h-full p-3 text-xl text-black border-0 shadow-none resize-none bg-slate-300 min-h-12 focus-visible:ring-0"
+            />
+            <Button
+              disabled={!!loading}
+              type="submit"
+              className="ml-auto h-[75px] gap-1.5"
             >
-              <Label htmlFor="message" className="sr-only">
-                Message
-              </Label>
-              <Textarea
-                id="message"
-                value={msg}
-                onChange={(e) => setMsg(e.target.value)}
-                placeholder="Type your message here..."
-                className="p-3 border-0 shadow-none resize-none min-h-12 focus-visible:ring-0"
-              />
-              <div className="flex items-center p-3 pt-0">
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Paperclip className="size-4" />
-                        <span className="sr-only">Attach file</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Attach File</TooltipContent>
-                  </Tooltip>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" size="icon">
-                        <Mic className="size-4" />
-                        <span className="sr-only">Use Microphone</span>
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent side="top">Use Microphone</TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-                <Button
-                  disabled={!!loading}
-                  type="submit"
-                  size="sm"
-                  className="ml-auto gap-1.5"
-                >
-                  {loading ? "Loading..." : "Send"}
-                  <CornerDownLeft className="size-3.5" />
-                </Button>
-              </div>
-            </form>
+              {loading ? "Loading..." : "Send"}
+              <CornerDownLeft className="size-3.5" />
+            </Button>
           </div>
-        </div>
-      </main>
-    </div>
+        </form>
+      </div>
+    </main>
   );
 }
 
