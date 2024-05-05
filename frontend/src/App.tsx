492403dbae0698970cabe987bc9e0429467b7b31
sani <ysani095@gmail.com>
2024-05-05 17:13:44 +0800

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,7 @@
-import {
-  Book,
-  Bot,
-  Code2,
-  CornerDownLeft,
-  LifeBuoy,
-  Mic,
-  Paperclip,
-  Settings,
-  Settings2,
-  Share,
-  SquareTerminal,
-  SquareUser,
-  Triangle,
-} from "lucide-react";
-
+import { CornerDownLeft, Mic, Paperclip } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
 import { Label } from "@/components/ui/label";
-
 import { Textarea } from "@/components/ui/textarea";
 import {
   Tooltip,
@@ -39,6 +15,7 @@ import { FormEventHandler, useState } from "react";
 function App() {
   const [history, setHistory] = useState<any[]>([]);
   const [msg, setMsg] = useState(" ");
+  const [loading, setLoading] = useState(false);
   // console.log(msg);
   // console.log(history);
   //  setHistory(() => [
@@ -65,6 +42,7 @@ function App() {
   ];
   const sentMessage: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const res = await axios.post("https://chatbot-5thp.onrender.com/ask/ai", {
         history,
@@ -86,238 +64,96 @@ function App() {
         ]);
       }
       setMsg(" ");
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       setMsg(" ");
       console.log(error);
     }
   };
 
   return (
-    <div className="grid h-screen w-full pl-[53px] ">
-      <aside className="fixed left-0 z-20 flex flex-col h-full border-r inset-y">
-        <div className="p-2 border-b">
-          <Button variant="outline" size="icon" aria-label="Home">
-            <Triangle className="size-5 fill-foreground" />
-          </Button>
-        </div>
-        <nav className="grid gap-1 p-2">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
+    <div className="flex items-center h-screen">
+      <main className="grid gap-4 overflow-auto w-[90%] lg:w-[60%] mx-auto">
+        <div className="relative flex flex-col justify-between p-4 border h-[700px] rounded-xl bg-muted/50">
+          <Badge variant="outline" className="absolute right-3 top-3">
+            Output
+          </Badge>
+          <ul className="flex flex-col gap-5 p-5 pb-16 mb-20">
+            {history.map((text: any, index) => {
+              return (
+                <>
+                  {text.role === "user" ? (
+                    <li key={index} className="flex flex-row">
+                      <div className="flex p-4 shadow-md rounded-xl bg-background">
+                        <p className="text-primary">{text.parts[0].text}</p>
+                      </div>
+                    </li>
+                  ) : (
+                    <li key={index} className="flex">
+                      <div className="flex w-3/4 p-4 shadow-md rounded-xl">
+                        <p className="text-primary">
+                          <span className="font-bold">{text.role}: </span>
+                          {text.parts[0].text}
+                        </p>
+                      </div>
+                    </li>
+                  )}
+                </>
+              );
+            })}
+          </ul>
+          <div className="bg-background">
+            <form
+              onSubmit={sentMessage}
+              className="overflow-hidden rounded-lg bg-background focus-within:ring-1 focus-within:ring-ring"
+              x-chunk="dashboard-03-chunk-1"
+            >
+              <Label htmlFor="message" className="sr-only">
+                Message
+              </Label>
+              <Textarea
+                id="message"
+                value={msg}
+                onChange={(e) => setMsg(e.target.value)}
+                placeholder="Type your message here..."
+                className="p-3 border-0 shadow-none resize-none min-h-12 focus-visible:ring-0"
+              />
+              <div className="flex items-center p-3 pt-0">
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <Paperclip className="size-4" />
+                        <span className="sr-only">Attach file</span>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Attach File</TooltipContent>
+                  </Tooltip>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <Mic className="size-4" />
+                        <span className="sr-only">Use Microphone</span>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">Use Microphone</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
                 <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg bg-muted"
-                  aria-label="Playground"
+                  disabled={!!loading}
+                  type="submit"
+                  size="sm"
+                  className="ml-auto gap-1.5"
                 >
-                  <SquareTerminal className="size-5" />
+                  {loading ? "Loading..." : "Send"}
+                  <CornerDownLeft className="size-3.5" />
                 </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Ai
-              </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Models"
-                >
-                  <Bot className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Models
-              </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="API"
-                >
-                  <Code2 className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                API
-              </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Documentation"
-                >
-                  <Book className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Documentation
-              </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Settings"
-                >
-                  <Settings2 className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Settings
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </nav>
-        <nav className="grid gap-1 p-2 mt-auto">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="mt-auto rounded-lg"
-                  aria-label="Help"
-                >
-                  <LifeBuoy className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Help
-              </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="mt-auto rounded-lg"
-                  aria-label="Account"
-                >
-                  <SquareUser className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Account
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </nav>
-      </aside>
-      <div className="flex flex-col">
-        <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
-          <h1 className="text-xl font-semibold">Ai</h1>
-          <Drawer>
-            <DrawerTrigger asChild>
-              <Button variant="ghost" size="icon" className="md:hidden">
-                <Settings className="size-4" />
-                <span className="sr-only">Settings</span>
-              </Button>
-            </DrawerTrigger>
-            <DrawerContent className="max-h-[80vh]">
-              <DrawerHeader>
-                <DrawerTitle>Configuration</DrawerTitle>
-                <DrawerDescription>
-                  Configure the settings for the model and messages.
-                </DrawerDescription>
-              </DrawerHeader>
-            </DrawerContent>
-          </Drawer>
-          <Button
-            variant="outline"
-            size="sm"
-            className="ml-auto gap-1.5 text-sm"
-          >
-            <Share className="size-3.5" />
-            Share
-          </Button>
-        </header>
-        <main className="grid flex-1 h-full gap-4 overflow-auto">
-          <div className="relative border flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-            <Badge variant="outline" className="absolute right-3 top-3">
-              Output
-            </Badge>
-            <ul className="flex flex-col flex-1 gap-5 p-5 pb-16 mb-20">
-              {history.map((text: any, index) => {
-                return (
-                  <>
-                    {text.role === "user" ? (
-                      <li key={index} className="flex flex-row">
-                        <div className="flex p-4 shadow-md rounded-xl bg-background">
-                          <p className="text-primary">{text.parts[0].text}</p>
-                        </div>
-                      </li>
-                    ) : (
-                      <li key={index} className="flex">
-                        <div className="flex w-3/4 p-4 shadow-md rounded-xl">
-                          <p className="text-primary">
-                            <span className="font-bold">{text.role}: </span>
-                            {text.parts[0].text}
-                          </p>
-                        </div>
-                      </li>
-                    )}
-                  </>
-                );
-              })}
-            </ul>
-            <div className="fixed pb-5 bottom-2 right-5 left-16 bg-background">
-              <form
-                onSubmit={sentMessage}
-                className="overflow-hidden rounded-lg bg-background focus-within:ring-1 focus-within:ring-ring"
-                x-chunk="dashboard-03-chunk-1"
-              >
-                <Label htmlFor="message" className="sr-only">
-                  Message
-                </Label>
-                <Textarea
-                  id="message"
-                  value={msg}
-                  onChange={(e) => setMsg(e.target.value)}
-                  placeholder="Type your message here..."
-                  className="p-3 border-0 shadow-none resize-none min-h-12 focus-visible:ring-0"
-                />
-                <div className="flex items-center p-3 pt-0">
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <Button variant="ghost" size="icon">
-                          <Paperclip className="size-4" />
-                          <span className="sr-only">Attach file</span>
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent side="top">Attach File</TooltipContent>
-                    </Tooltip>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <Button variant="ghost" size="icon">
-                          <Mic className="size-4" />
-                          <span className="sr-only">Use Microphone</span>
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent side="top">Use Microphone</TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                  <Button type="submit" size="sm" className="ml-auto gap-1.5">
-                    Send Message
-                    <CornerDownLeft className="size-3.5" />
-                  </Button>
-                </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   );
 }
